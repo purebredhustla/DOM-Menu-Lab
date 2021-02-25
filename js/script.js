21a221b643b2237a47215d97086b3dbdbb5fa9a6
Ativut Son <nazareth_0@hotmail.com>
2021-02-24 19:30:46 -0500

    Second commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+// Menu data structure
+var menuLinks = [
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '/catalog'},
+  {text: 'orders', href: '/orders'},
+  {text: 'account', href: '/account'},
+];
+
 const mainEl = document.querySelector('main');
 mainEl.style.backgroundColor = '#4a4e4d';
 mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
@@ -8,13 +16,8 @@ topMenuEl.style.height = '100%';
 topMenuEl.style.backgroundColor = '#0e9aa7';
 topMenuEl.classList.add('flex-around');
 
-// Menu data structure
-var menuLinks = [
-    {text: 'about', href: '/about'},
-    {text: 'catalog', href: '/catalog'},
-    {text: 'orders', href: '/orders'},
-    {text: 'account', href: '/account'},
-  ];
-
-  const aEl = document.createElement('a');
-  aEl.setAttribute('href', )
+menuLinks.forEach(link => {
+const aEl = document.createElement('a');
+aEl.href = link.href;
+aEl.innerHTML = link.text;
+topMenuEl.appendChild(aEl);});
